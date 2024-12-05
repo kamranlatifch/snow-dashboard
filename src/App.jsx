728cb5f788685523258b67dc9e97d4be6ba1eb6b
kamranlatifch <kamranlatif98@gmail.com>
2024-12-05 14:21:45 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from '@components';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className='bg-red-600'>hello</p>
+    </>
+  );
+}
+
+export default App;
