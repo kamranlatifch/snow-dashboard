bcef3842d1610b524f4a08d0baf3d5786de41252
Muhammad Kamran Latif <111248908+kamranlatifch@users.noreply.github.com>
2024-12-09 16:01:08 +0500

    feat/leftsidebar (#3)

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -191,11 +191,10 @@ const Sidebar = React.forwardRef(
         </Sheet>
       );
     }
-
     return (
       <div
         ref={ref}
-        className='group peer  md:block text-sidebar-foreground'
+        className='group peer   md:block text-sidebar-foreground'
         data-state={state}
         data-collapsible={state === 'collapsed' ? collapsible : ''}
         data-variant={variant}
@@ -204,7 +203,7 @@ const Sidebar = React.forwardRef(
         {/* This is what handles the sidebar gap on desktop */}
         <div
           className={cn(
-            'duration-200 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] ease-linear',
+            'duration-200  relative h-svh w-[--sidebar-width] bg-transparent transition-[width] ease-linear',
             'group-data-[collapsible=offcanvas]:w-0',
             'group-data-[side=right]:rotate-180',
             variant === 'floating' || variant === 'inset'
@@ -214,7 +213,8 @@ const Sidebar = React.forwardRef(
         />
         <div
           className={cn(
-            'duration-200 fixed inset-y-0 z-10  h-svh w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex',
+            'duration-200  fixed inset-y-0 z-10  h-svh w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex',
+            state == 'collapsed' ? 'p-2' : 'p-5',
             side === 'left'
               ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
               : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
@@ -228,7 +228,7 @@ const Sidebar = React.forwardRef(
         >
           <div
             data-sidebar='sidebar'
-            className='flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow'
+            className='flex  h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow'
           >
             {children}
           </div>
@@ -456,7 +456,7 @@ const SidebarMenuItem = React.forwardRef(({ className, ...props }, ref) => (
 SidebarMenuItem.displayName = 'SidebarMenuItem';
 
 const sidebarMenuButtonVariants = cva(
-  'peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
+  'peer/menu-button flex items-center gap-1 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8  aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
   {
     variants: {
       variant: {
--- src/pages/LeftSideBar.jsx
+++ src/pages/LeftSideBar.jsx
@@ -7,54 +7,294 @@ import {
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenuSubItem,
+  SidebarMenuSub,
+  Avatar,
+  AvatarImage,
+  Collapsible,
+  CollapsibleTrigger,
+  AvatarFallback,
+  CollapsibleContent,
+  SidebarMenuSubButton,
+  Tabs,
+  TabsTrigger,
+  TabsList,
+  TabsContent,
+  useSidebar,
 } from '@components';
-import { Home, Settings, HelpCircle, PersonStanding } from 'lucide-react';
+import {
+  Home,
+  Settings,
+  HelpCircle,
+  PersonStanding,
+  Users,
+  ChevronRight,
+} from 'lucide-react';
+import Image from '../assets/images/ByeWind.png';
+
+import {
+  Dot,
+  Default,
+  Ecommerce,
+  Projects,
+  OnlineCourse,
+  UserProfile,
+  Account,
+  Corporate,
+  Blog,
+  Socials,
+  Snow,
+  SnowIcon,
+} from '@assets/svgs';
 
 const LeftSidebar = () => {
+  const menuItems = [
+    {
+      groupLabel: 'Dashboard',
+      subGroups: [
+        {
+          title: 'Default',
+          icon: Default,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Ecommerce',
+          icon: Ecommerce,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Projects',
+          icon: Projects,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Online Courses',
+          icon: OnlineCourse,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+      ],
+    },
+    {
+      groupLabel: 'Pages',
+      subGroups: [
+        {
+          title: 'User Profile',
+          icon: UserProfile,
+          items: [
+            { title: 'Overview', href: '#' },
+
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Documents', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Account',
+          icon: Account,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Corporate',
+          icon: Corporate,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Blog',
+          icon: Blog,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+        {
+          title: 'Social',
+          icon: Socials,
+          items: [
+            { title: 'Overview', href: '#' },
+            { title: 'Analytics', href: '#' },
+            { title: 'Projects', href: '#' },
+            { title: 'Campaigns', href: '#' },
+            { title: 'Followers', href: '#' },
+          ],
+        },
+      ],
+    },
+  ];
+  const {
+    state,
+    open,
+    setOpen,
+    openMobile,
+    setOpenMobile,
+    isMobile,
+    toggleSidebar,
+  } = useSidebar();
+  console.log('State in left sidebar is ', state);
   return (
-    <Sidebar defaultOpen={true} variant='sidebar' collapsible='icon'>
-      <SidebarHeader>
+    <Sidebar
+      className=' bg-base'
+      defaultOpen={true}
+      variant='sidebar'
+      collapsible='icon'
+    >
+      <SidebarHeader className='bg-base p-0'>
         <SidebarMenu>
-          <SidebarMenuItem className='ml-4'>
+          <SidebarMenuItem className=''>
             <SidebarMenuButton asChild>
               <a href='#home'>
-                <PersonStanding />
-                <span>Left Sidebar</span>
+                <Avatar className='w-6 h-6'>
+                  <AvatarImage src={Image} />
+                  <AvatarFallback>BW</AvatarFallback>
+                </Avatar>
+
+                <span className='font-inter font-normal text-sm'>ByeWind</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
-      <SidebarContent>
+      <SidebarContent className='bg-base mt-4'>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              <a href='#home'>
-                <Home />
-                <span>Home</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              <a href='#settings'>
-                <Settings />
-                <span>Settings</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              <a href='#help'>
-                <HelpCircle />
-                <span>Help</span>
-              </a>
-            </SidebarMenuButton>
+            {state === 'expanded' && (
+              <Tabs defaultValue='account' className='bg-transparent px-3'>
+                <TabsList className='bg-transparent flex gap-3 justify-start'>
+                  <TabsTrigger
+                    value='account'
+                    className='font-inter font-normal text-sm'
+                  >
+                    Favorites
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value='password'
+                    className='font-inter font-normal text-sm'
+                  >
+                    Recently
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent value='account' className='mt-1'>
+                  <li className='flex items-center py-1 gap-2 font-inter text-sm font-normal text-text_primary'>
+                    <Dot />
+                    OverView
+                  </li>
+                  <li className='flex items-center py-1 gap-2 mt-1 font-inter text-sm font-normal text-text_primary'>
+                    <Dot />
+                    Projects
+                  </li>
+                </TabsContent>
+                <TabsContent value='password' className='mt-1'>
+                  <li className='flex items-center  py-1 gap-2 font-inter text-sm font-normal text-text_primary'>
+                    <Dot />
+                    Count
+                  </li>
+                  <li className='flex items-center gap-2  py-1 mt-1 font-inter text-sm font-normal text-text_primary'>
+                    <Dot />
+                    Pages
+                  </li>
+                </TabsContent>
+              </Tabs>
+            )}
           </SidebarMenuItem>
         </SidebarMenu>
+
+        <SidebarGroup>
+          {menuItems.map((data, index) => (
+            <div key={index}>
+              <SidebarGroupLabel className='font-inter px-4 font-normal text-sm text-text_secondary'>
+                {data.groupLabel}
+              </SidebarGroupLabel>
+              <SidebarMenu>
+                {data.subGroups.map((item) => (
+                  <Collapsible
+                    key={item.title}
+                    asChild
+                    className='group/collapsible'
+                  >
+                    <SidebarMenuItem className='p-0'>
+                      <CollapsibleTrigger asChild className='p-0 mt-1'>
+                        <SidebarMenuButton
+                          variant='default'
+                          tooltip={item.title}
+                          className='relative'
+                          // className='w-full'
+                        >
+                          {/* <div className='absolute hidden w-1 h-4 bg-black group-data-[state=open]/collapsible:flex rounded'></div> */}
+                          {state === 'expanded' && (
+                            <ChevronRight
+                              className={`ml-auto ${
+                                state === 'collapsed' ? 'hidden' : ''
+                              }size-4 text-text_secondary_light transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90`}
+                            />
+                          )}{' '}
+                          <item.icon className='size-4' />
+                          <span className='font-inter  text-sm text-text_primary'>
+                            {item.title}
+                          </span>
+                        </SidebarMenuButton>
+                      </CollapsibleTrigger>
+                      <CollapsibleContent>
+                        <SidebarMenuSub className='border-none'>
+                          {item.items.map((subItem) => (
+                            <SidebarMenuSubItem key={subItem.title}>
+                              <SidebarMenuSubButton asChild>
+                                <a href={subItem.href}>{subItem.title}</a>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </SidebarMenuItem>
+                  </Collapsible>
+                ))}
+              </SidebarMenu>
+            </div>
+          ))}
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <p className='px-4 text-sm text-gray-500'>© 2023 Your Company</p>
+      <SidebarFooter className='bg-base items-center'>
+        {state === 'collapsed' ? <SnowIcon className='w-5 h-5' /> : <Snow />}
       </SidebarFooter>
     </Sidebar>
   );
